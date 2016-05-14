b7392640f244157f6ccafd9944a7914ebff8a493
iwaken71 <iwaken59371518@gmail.com>
2016-05-14 17:42:34 +0900

    photon

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RoomManager : Photon.MonoBehaviour {
+
+	void Start ()
+	{
+		//photonを利用するための初期設定 ロビーを作成して入る？
+		PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+
+	//PhotonNetwork.ConnectUsingSettingsを行うと呼ばれる
+	void OnJoinedLobby()
+	{
+		//ランダムにルームに入る
+		PhotonNetwork.JoinRandomRoom();
+	}
+
+	//ランダムにルームに入れなかった
+	void OnPhotonRandomJoinFailed()
+	{
+		//部屋を自分で作って入る
+		PhotonNetwork.CreateRoom(null);
+	}
+
+	//ルームに入室成功
+	void OnJoinedRoom()
+	{
+		//キャラクター作成
+		PlayerMake ();
+
+	}
+
+	void PlayerMake(){
+		PhotonNetwork.Instantiate ("Player",Vector3.zero,Quaternion.identity,0);
+	}
+}
